16f02f8eaddf38187b7f904d99364c32b970ad54
Alice Hsing <alicehsing@gmail.com>
2022-03-17 12:57:58 -0700

    setup SQL tables in postgres

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS dogs;
+
+CREATE TABLE dogs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INT NOT NULL CHECK (age > -1),
+    favorite_treat TEXT
+);
+
+INSERT INTO
+    dogs (name, age, favorite_treat)
+
+VALUES
+    ('Gus', 3, 'Chicken Strips'),
+    ('Cooper', 10, 'Salmon');
