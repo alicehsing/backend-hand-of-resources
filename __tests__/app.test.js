77be20c6148b517f28f0e9bd74bedd418a03b69c
Alice Hsing <alicehsing@gmail.com>
2022-03-18 15:31:33 -0700

    added create endpoint to api/v1/songs

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,98 +11,110 @@ describe('backend-hand-of-resources routes', () => {
 
   afterAll(() => {
     pool.end();
-  })
-
-.skip('creates an instance of Dog to dogs table', async() => {
-  const expected = {
-    name: 'Momo',
-    age: 1,
-    favoriteTreat: 'Beef Jerky'
-  }
-
-  const res = await request(app)
-  .post('/api/v1/dogs')
-  .send(expected);
-
-  expect(res.body).toEqual({ 
-    id: expect.any(String),
-    ...expected 
   });
-})
 
-.skip('returns an array of dogs', async() => {
-  const expected = await Dog.getAll();
-  const res = await request(app)
-  .get('/api/v1/dogs');
+  it.skip('creates an instance of Dog to dogs table', async() => {
+    const expected = {
+      name: 'Momo',
+      age: 1,
+      favoriteTreat: 'Beef Jerky'
+    };
 
-  expect(res.body).toEqual(expected)
-})
+    const res = await request(app)
+      .post('/api/v1/dogs')
+      .send(expected);
 
-.skip('returns a single dog by Id', async() => {
-  const expected = await Dog.getById(1);
-  const res = await request(app)
-  .get(`/api/v1/dogs/${expected.id}`);
+    expect(res.body).toEqual({ 
+      id: expect.any(String),
+      ...expected 
+    });
+  });
 
-  expect(res.body).toEqual(expected)
-})
+  it.skip('returns an array of dogs', async() => {
+    const expected = await Dog.getAll();
+    const res = await request(app)
+      .get('/api/v1/dogs');
 
-.skip('deletes a dog by Id', async() => {
-  // Creates a new dog in DB
-  const newDog = await Dog.insert({ 
-    name: 'Chomp',
-    age: 5,
-    favoriteTreat: 'Rawhide'
-  })
-  // Get the new dog as the expected
-  const expected = await Dog.getById(newDog.id);
-  // Delete the dog that was just inserted
-  const res = await request(app)
-  .delete(`/api/v1/dogs/${expected.id}`);
+    expect(res.body).toEqual(expected);
+  });
+
+  it.skip('returns a single dog by Id', async() => {
+    const expected = await Dog.getById(1);
+    const res = await request(app)
+      .get(`/api/v1/dogs/${expected.id}`);
+
+    expect(res.body).toEqual(expected);
+  });
+
+  it.skip('deletes a dog by Id', async() => {
+    // Creates a new dog in DB
+    const newDog = await Dog.insert({ 
+      name: 'Chomp',
+      age: 5,
+      favoriteTreat: 'Rawhide'
+    });
+      // Get the new dog as the expected
+    const expected = await Dog.getById(newDog.id);
+    // Delete the dog that was just inserted
+    const res = await request(app)
+      .delete(`/api/v1/dogs/${expected.id}`);
   
-  expect(res.body).toEqual(expected)
-})
+    expect(res.body).toEqual(expected);
+  });
 
-.skip('updates a dog by id', async() => {
-  const expected = {
-    id: expect.any(String),
-    name: 'Gus',
-    age: 4,
-    favoriteTreat: 'Chicken Strips'
-  }
+  it.skip('updates a dog by id', async() => {
+    const expected = {
+      id: expect.any(String),
+      name: 'Gus',
+      age: 4,
+      favoriteTreat: 'Chicken Strips'
+    };
 
-  const res = await request(app)
-  .patch(`/api/v1/dogs/1`)
-  .send({ age: 4 });
+    const res = await request(app)
+      .patch('/api/v1/dogs/1')
+      .send({ age: 4 });
 
-  expect(res.body).toEqual(expected);
+    expect(res.body).toEqual(expected);
 
 
-  // Alternative syntax when there isn't hardcoded values in SQL.
-  // Insert a new instance first in order to make changes.
+    // Alternative syntax when there isn't hardcoded values in SQL.
+    // Insert a new instance first in order to make changes.
 
-  // const dog = await Dog.insert({
-  //   name: 'Momo',
-  //   age: 6,
-  //   favoriteTreat: 'Beef Jerky'
-  // });
+    // const dog = await Dog.insert({
+    //   name: 'Momo',
+    //   age: 6,
+    //   favoriteTreat: 'Beef Jerky'
+    // });
 
-  // const res = await request(app)
-  // .patch(`/api/v1/dogs/${dog.id}`)
-  // .send({ age: 7 });
+    // const res = await request(app)
+    // .patch(`/api/v1/dogs/${dog.id}`)
+    // .send({ age: 7 });
 
-  // const expected = { 
-  //   id: expect.any(String),
-  //   ...dog,
-  //   age: 7
-  // };
+    // const expected = { 
+    //   id: expect.any(String),
+    //   ...dog,
+    //   age: 7
+    // };
 
-  // expect(res.body).toEqual(expected);
-})
+    // expect(res.body).toEqual(expected);
+  });
 
-it('creates an instance of song to songs table', async() => {
-  const expected = {
+  it('creates an instance of song to songs table', async() => {
+    const expected = {
+      title: 'In My Blood',
+      artist: 'Shawn Mendes',
+      album: 'Shawn Mendes'
+    };
+
+    const response = await request(app)
+      .post('/api/v1/songs')
+      .send(expected);
     
-  }
-})
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      ...expected
+    });
+  });
 
+  
 });
