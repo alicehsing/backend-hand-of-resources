1e2ecf8b002ff63295773cc38c7ba29b98c9b45c
Alice Hsing <alicehsing@gmail.com>
2022-03-21 19:24:51 -0700

    created movies table in SQL and added api routes in app.js

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,7 @@
 DROP TABLE IF EXISTS dogs;
 DROP TABLE IF EXISTS songs;
 DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS movies;
 
 CREATE TABLE dogs (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -43,3 +44,17 @@ INSERT INTO
 VALUES
     ('Full', 'Julia Styro', 'Lake Union Publishing'),
     ('The Judge''s List', 'John Grisham', ' Doubleday');
+
+
+CREATE TABLE movies (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    director TEXT NOT NULL,
+    year_published INT NOT NULL
+);
+
+INSERT INTO
+    movies (title, director, year_published)
+VALUES
+    ('The Godfather', 'Francis Ford Coppola', 1972),
+    ('The Skeleton Key', 'Iain Softley', 2005);
